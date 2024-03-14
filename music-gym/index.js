2108e33b998e913575c68b8fb5f2ac2f8e866517
mic-sinna <micsinna2301@gmail.com>
2024-03-14 16:41:22 +0800

    Added onclick logic for the exercise cards button.

--- music-gym/index.js
+++ music-gym/index.js
@@ -1,3 +1,3 @@
 for (let node of document.querySelectorAll(".exercise")) {
-    node.onclick = () => { window.location.assign("/pages/music-gym/lol"); };
+    node.onclick = () => { window.location.assign("./pages/music-gym/lol"); };
 }
