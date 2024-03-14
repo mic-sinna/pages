8acaedd83aa04930a8647e1bb2218493067a03ec
mic-sinna <micsinna2301@gmail.com>
2024-03-14 16:56:10 +0800

    Fixed the redirection logic for the exercise card buttons.

--- music-gym/index.js
+++ music-gym/index.js
@@ -1,3 +1,5 @@
+console.log("JS Loaded.");
+
 for (let node of document.querySelectorAll(".exercise")) {
     node.onclick = () => {
         window.location.assign("https://mic-sinna.github.io/pages/music-gym/lol");
