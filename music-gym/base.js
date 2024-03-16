81d8d7e2ba9886389b63cd47c894beeb5afa5d0c
mic-sinna <micsinna2301@gmail.com>
2024-03-16 22:26:27 +0800

    For Music Gym, added a play/stop button for the Flash Chords exercise.

--- music-gym/base.js
+++ music-gym/base.js
@@ -1,1 +1,62 @@
 const rootPath = "https://mic-sinna.github.io/pages/music-gym";
+
+function toneToString(tone) {
+    if (typeof(tone) != "number" || tone % 1 != 0 || 
+        tone < 0 || tone > 11
+    ) {
+        throw new Error("The parameter 'tone' must be an integer between 0 and 11.");
+    } else {
+        let toneNotations;
+        switch(tone) {
+            case 0:
+                toneNotations = ["C"];
+                break;
+            case 1:
+                toneNotations = ["C\u266F","D\u266D"];
+                break;
+            case 2:
+                toneNotations = ["D"];
+                break;
+            case 3:
+                toneNotations = ["D\u266F","E\u266D"];
+                break;
+            case 4:
+                toneNotations = ["E"];
+                break;
+            case 5:
+                toneNotations = ["F"];
+                break;
+            case 6:
+                toneNotations = ["F\u266F","G\u266D"];
+                break;
+            case 7:
+                toneNotations = ["G"];
+                break;
+            case 8:
+                toneNotations = ["G\u266F","A\u266D"];
+                break;
+            case 9:
+                toneNotations = ["A"];
+                break;
+            case 10:
+                toneNotations = ["A\u266F","B\u266D"];
+                break;
+            case 11:
+                toneNotations = ["B"];
+                break;
+        }
+        return toneNotations;
+    }
+}
+
+function sleep(duration) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                resolve();
+            } catch {
+                reject();
+            }
+        }, duration);
+    });
+}
--- music-gym/flash-chords.js
+++ music-gym/flash-chords.js
@@ -1,3 +1,31 @@
 document.getElementById("back-btn").onclick = () => {
     window.location.assign(rootPath)
 };
+
+const chordDisplay = document.getElementById("chord");
+
+let displayChangeInterval = 2000;
+let playing = false;
+
+const playBtn = document.getElementById("play-btn");
+const playBtnTxt = document.querySelector("#play-btn p");
+playBtn.addEventListener("click", () => {
+    playing = !playing;
+    if (playing == true) {
+        playerLoop();
+        playBtnTxt.textContent = "STOP";
+    } else {
+        playBtnTxt.textContent = "PLAY";
+    }
+});
+
+const playerLoop = (async () => {
+    while (playing) {
+        const toneNotations = toneToString(Math.floor(Math.random() * 12))
+        const strRootTone = toneNotations[toneNotations.length > 1 ? Math.floor(Math.random() * toneNotations.length) : 0];
+        const strChordMode = ["", "m"][Math.floor(Math.random() * 2)];
+        chordDisplay.textContent = `${strRootTone}${strChordMode}`;
+        await sleep(displayChangeInterval);
+    }
+    chordDisplay.textContent = "";
+});
